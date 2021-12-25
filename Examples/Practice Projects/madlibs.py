d6b5a8e6e8ba1ca2f2595e6e608a4796b7438ad5
Daniel Borenstein <borenstein2000@gmail.com>
2021-12-25 03:30:49 -0600

    projects to practice my understandings

---
+++ Examples/Practice Projects/madlibs.py
@@ -0,0 +1,13 @@
+adj = input("Adjective: ")
+place = input("Place: ")
+rapper = input("Rapper: ")
+negative_adj = ("Negative Adjective")
+
+madlib = f"A(n) {negative_adj} force following the lightning went throughout {place}, \
+bringing every test material to life. The transfigured bluebooks and scantrons heard {rapper} \
+from nearby headphones and started spitting {adj} bars."
+
+print(madlib)
+ 
+
+
